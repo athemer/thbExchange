39adc4e80383e0f885825931333caa24a0fe960f
kuanhuachen <kuanhuachen@doits-MacBook-Pro.local>
2017-08-11 10:35:53 +0800

    加入 kanna / alamofire

---
+++ Pods/Target Support Files/Pods-thbExchangeUITests/Pods-thbExchangeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_thbExchangeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_thbExchangeUITests
+@end
--- thbExchange/ViewController.swift
+++ thbExchange/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
+
 
 class ViewController: UIViewController {
 
